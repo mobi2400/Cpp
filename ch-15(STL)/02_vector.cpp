42caca1de1758aa0b5360624dd055490034777ac
mobasshir <mobasshirkhan9931@gmail.com>
2025-04-23 00:46:24 +0530

    vector use and functions

---
+++ ch-15(STL)/02_vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> v={10,20,30,40};
+    v.push_back(25);
+    v.push_back(70);
+    v.pop_back();
+    // for(int x:v){
+    //     cout<<x<<" ";
+    // }
+    vector<int>:: iterator itr = v.begin();
+    for(itr=v.begin();itr!=v.end();itr++)
+        cout<<*itr<<" ";
+
+    return 0;
+}
