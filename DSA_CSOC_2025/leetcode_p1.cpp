0d63831696ab9d78e6256fa721131593c7238408
mobasshir <mobasshirkhan9931@gmail.com>
2025-04-24 20:28:49 +0530

    trying vector

--- DSA_CSOC_2025/leetcode_p1.cpp
+++ DSA_CSOC_2025/leetcode_p1.cpp
@@ -1,35 +1,21 @@
 /*Write a function that reverses a string. The input string is given as an array of characters s. You must do this by modifying the input array in-place with O(1) extra memory.*/
 
 
-#include <iostream>
+// 
+#include <bits/stdc++.h>
 using namespace std;
-int  main(){
-   int length;
-
-   cout<<"enter the length of an array"<<endl;
-   cin>>length;
-   int A[length];
-   cout<<"enter the array"<<endl;
-   for (int i = 0; i < length; i++)
-   {
-        cin>>A[i];
-   }
-   int goodPairs =0;
-   for (int i = 0; i < length; i++)
-   {
-    for (int j = i+1; j < length; j++)
-    {
-        if (A[i]==A[j])
-        {
-            goodPairs++;
-        }
-        
-    }
+int main(){
+    vector <int> A;
+    int goodpoints =0;
+    cin>>A;
+    vector<int>:: iterator itr;
+    vector<int>:: iterator itr1;
+    for(itr=v.begin();itr!=A.end();itr++){
+        for(itr1=itr+1;itr1!=A.end();itr1++){
+            if(A[itr]==A[itr1]){
+                goodpoints++;
+            }
     
-   }
-    cout<<goodPairs;
-
-
-
-    return 0;
+}}
+cout<<goodpoints;
 }
